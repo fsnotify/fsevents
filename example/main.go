e05290313c6e972f40404b01a856c702d28316e0
Drew Wells <drew.wells00@gmail.com>
2016-04-21 12:08:30 -0500

    Pushes test coverage just over 50%
    fix vet errors in fsevtCallback
    separate all parts that call C code into separate wrap file
    test registry functions
    test creating of paths
    test inspects StreamRef to ensure setupStream created it correctly

--- example/main.go
+++ example/main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"runtime"
@@ -13,17 +14,23 @@ import (
 )
 
 func main() {
-	dev, _ := fsevents.DeviceForPath("/tmp")
+	path := "/tmp"
+	dev, err := fsevents.DeviceForPath(path)
+	if err != nil {
+		log.Println("Failed to retrieve device for path:", err)
+	}
 	log.Print(dev)
 	log.Println(fsevents.EventIDForDeviceBeforeTime(dev, time.Now()))
 
 	es := &fsevents.EventStream{
-		Paths:   []string{"/tmp"},
+		Paths:   []string{path},
 		Latency: 500 * time.Millisecond,
-		// Device:  dev,
-		Flags: fsevents.FileEvents | fsevents.WatchRoot}
+		Device:  dev,
+		Flags:   fsevents.FileEvents | fsevents.WatchRoot}
 	es.Start()
 	ec := es.Events
+
+	fmt.Println("Device UUID", fsevents.GetDeviceUUID(dev))
 
 	go func() {
 		for msg := range ec {
