995f15ce7401254591ca19d62e6a02c2650d6484
Lachlan Donald <lachlan@ljd.cc>
2017-09-30 12:09:21 +1000

    Add test to prove data race

--- fsevents_test.go
+++ fsevents_test.go
@@ -1,3 +1,32 @@
 // +build darwin
 
 package fsevents
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestBasicExample(t *testing.T) {
+	path, err := ioutil.TempDir("", "fsexample")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(path)
+
+	dev, err := DeviceForPath(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	es := &EventStream{
+		Paths:   []string{path},
+		Latency: 500 * time.Millisecond,
+		Device:  dev,
+		Flags:   FileEvents,
+	}
+
+	es.Start()
+}
