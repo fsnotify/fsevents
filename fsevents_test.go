ed9a50f8badcd11817e4fa3044b6765bb54219db
Lachlan Donald <lachlan@ljd.cc>
2018-02-23 17:02:18 +1100

    Avoid race in channel creation, closes #28 (#29)
    * Add test to prove data race
    
    * Make failing test more complete example
    
    * Move channel creation earlier to avoid races

--- fsevents_test.go
+++ fsevents_test.go
@@ -1,3 +1,52 @@
 // +build darwin
 
 package fsevents
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestBasicExample(t *testing.T) {
+	path, err := ioutil.TempDir("", "fsexample")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(path)
+
+	dev, err := DeviceForPath(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	es := &EventStream{
+		Paths:   []string{path},
+		Latency: 500 * time.Millisecond,
+		Device:  dev,
+		Flags:   FileEvents,
+	}
+
+	es.Start()
+
+	wait := make(chan Event)
+	go func() {
+		for msg := range es.Events {
+			for _, event := range msg {
+				t.Logf("Event: %#v", event)
+				wait <- event
+				es.Stop()
+				return
+			}
+		}
+	}()
+
+	err = ioutil.WriteFile(filepath.Join(path, "example.txt"), []byte("example"), 0700)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	<-wait
+}
