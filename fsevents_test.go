82eb4c8329872b52b27d4d404a77bcd1eb258b7a
Emanuel Skrenković <eskrenkovic@gmail.com>
2024-05-04 22:26:54 +0200

    add test for many concurrent actions

--- fsevents_test.go
+++ fsevents_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 )
@@ -198,5 +199,134 @@ func TestRegistry(t *testing.T) {
 	registry.Delete(i)
 	if registry.Get(i) != nil {
 		t.Error("failed to delete registry")
+	}
+}
+
+func TestMany(t *testing.T) {
+	tmp := t.TempDir()
+
+	path, err := filepath.EvalSymlinks(tmp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dev, err := DeviceForPath(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	es := &EventStream{
+		Paths:   []string{path},
+		Latency: 0,
+		Device:  dev,
+		Flags:   FileEvents | NoDefer,
+	}
+
+	err = es.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//   300 = 100 files in root folder, 3 events per
+	// + 300 = 100 files in subfolders, 3 events per
+	// + 200 = 100 subfolders, 2 events per
+	// + 1 root folder
+	expectedEventCount := 300 + 300 + 200 + 1
+
+	var doneWg sync.WaitGroup
+	doneWg.Add(expectedEventCount)
+
+	events := make(map[string]EventFlags, expectedEventCount)
+	go func() {
+		for msg := range es.Events {
+			for _, event := range msg {
+				if _, ok := events[event.Path]; !ok {
+					events[event.Path] = event.Flags
+				} else {
+					events[event.Path] = events[event.Path].set(event.Flags)
+				}
+
+				doneWg.Done()
+			}
+		}
+	}()
+
+	const data = "data"
+
+	var wg sync.WaitGroup
+	wg.Add(200)
+
+	for i := 0; i < 100; i++ {
+		i := i
+
+		go func() {
+			defer wg.Done()
+			echoAppend(t, data, fmt.Sprintf("%s/file-%d", path, i))
+		}()
+
+		go func() {
+			defer wg.Done()
+			mkdir(t, fmt.Sprintf("%s/dir-%d", path, i))
+		}()
+	}
+	wg.Wait()
+
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		i := i
+
+		go func() {
+			defer wg.Done()
+			echoAppend(t, data, fmt.Sprintf("%s/dir-%d/file-%d", path, i, i))
+		}()
+	}
+	wg.Wait()
+
+	wg.Add(200)
+	for i := 0; i < 100; i++ {
+		i := i
+		go func() {
+			defer wg.Done()
+			rm(t, fmt.Sprintf("%s/file-%d", tmp, i))
+		}()
+		go func() {
+			defer wg.Done()
+			rmAll(t, fmt.Sprintf("%s/dir-%d", tmp, i))
+		}()
+	}
+	wg.Wait()
+
+	doneWg.Wait()
+
+	const fileExpectedFlags = ItemIsFile | ItemCreated | ItemModified | ItemRemoved
+	const dirExpectedFlags = ItemIsDir | ItemCreated | ItemRemoved
+
+	for p, flags := range events {
+		if p == strings.TrimPrefix(path, "/") {
+			continue
+		}
+
+		switch {
+		case flags.hasFlag(ItemIsFile):
+			if flags.String() != fileExpectedFlags.String() {
+				t.Fatalf(
+					"file flags for path '%s' did not match expected '%s' found '%s'",
+					p,
+					fileExpectedFlags.String(),
+					flags.String(),
+				)
+			}
+		case flags.hasFlag(ItemIsDir):
+			if flags.String() != dirExpectedFlags.String() {
+				t.Fatalf(
+					"file flags for path '%s' did not match expected '%s' found '%s'",
+					p,
+					dirExpectedFlags.String(),
+					flags.String(),
+				)
+			}
+		default:
+			t.Fatal("unrecognized flag")
+		}
 	}
 }
